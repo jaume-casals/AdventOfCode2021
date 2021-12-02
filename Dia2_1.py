fda86c9b03808e2b3abeccddba30ec269d47a4ca
Jaume Casals <92870788+jaume-casals@users.noreply.github.com>
2021-12-02 13:50:27 +0100

    Create Dia2_1.py

---
+++ Dia2_1.py
@@ -0,0 +1,20 @@
+f = open("Input2.txt")
+
+inp = f.readlines()
+
+f.close()
+
+x = 0
+y = 0
+
+for l in inp:
+    op = l.split(" ",1)
+    if (op[0] == "forward"):
+        x += int(op[1])
+    elif (op[0] == "down"):
+        y += int(op[1])
+    else:
+       y -= int(op[1])
+
+print("x", x, "y", y)
+print("x*y = ", x*y)
