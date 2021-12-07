a7da334dc1451f4cb1dffddfcba9f34b0a4efca4
jaume.casals.vilaplana <jaume.casals.vilaplana@est.fib.upc.edu>
2021-12-07 00:27:11 -0800

    Dia 7 de 25

---
+++ Dia7_1.py
@@ -0,0 +1,32 @@
+f = open("Input7.txt")
+
+inp = f.readline()
+
+f.close()
+
+inp = inp.split(',')
+
+l = [int(x) for x in inp]
+
+max = l[0]
+min = l[0]
+
+for x in l:
+    if x > max:
+        max = x
+    if x < min:
+        min = x
+
+def fuel(l, pos):
+    cost = 0
+    for x in l:
+        cost += abs(x-pos)
+    return cost
+
+m = 9999999999999999999999999999999999999
+for i in range(min, max):
+    y = fuel(l, i)
+    if (y < m):
+        m = y
+
+print(m)
